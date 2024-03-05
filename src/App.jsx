d9dbbad6828e4e3030121451760ab94a27428078
Emily Gao Wang <egwang1@uci.edu>
2024-03-04 19:08:45 -0800

    Could flip random cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const pairs = [
+    {"question": "Hello", "answer": "你好"},
+    {"question": "Bye", "answer": "拜拜"},
+    {"question": "Good Morning", "answer": "早上好"},
+    {"question": "Good Afternoon", "answer": "中午好"},
+    {"question": "Good Evening", "answer": "晚上好"},
+    {"question": "Good Night", "answer": "晚安"},
+    {"question": "Merry Christma", "answer": "圣诞节快乐"},
+    {"question": "Happy New Year", "answer": "新年快乐"},
+    {"question": "I'm Hungry", "answer": "我饿了"},
+    {"question": "My Name is ...", "answer": "我的名字是..."}
+  ];
+                  
+  const [currIndex, setCurIndex] = useState(0);
+  const [showAnswer, setShowAnswer] = useState(false);
+
+  // whether flip card
+  const flipCard = () => setShowAnswer(!showAnswer);
+
+  // Choose random card
+  const showRandomCard = () => {
+    const randIndex = Math.floor(Math.random() * pairs.length);
+    setCurIndex(randIndex);
+    setShowAnswer(false);
+  }
 
   return (
-    <>
+    <div className='Container'>
+      <div className='Contents'>
+        <h1>Chinese Learning Flashcards!</h1>
+        <h2>Don't know how to start learning Chinese? Test to learn basic words here!</h2>
+        <h3>Number of cards: 10</h3>
+      </div>
+      <div className='Card' onClick={flipCard}>
+        <h4>{showAnswer ? pairs[currIndex].answer : pairs[currIndex].question}</h4>
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button className='button' onClick={showRandomCard}>Next</button> 
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
